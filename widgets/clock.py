10c43f9cdbce1df3e4dc9236c7d8b42937f15060
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-08-25 18:13:26 -0700

    remove black space at end of clock widget

--- widgets/clock.py
+++ widgets/clock.py
@@ -5,7 +5,7 @@ class ClockWidget(object):
     def output(self):
         return {
             'name': "clock",
-            'full_text': datetime.datetime.now().strftime(" %a %b %d %H:%M "),
-            'short_text': datetime.datetime.now().strftime(" %H:%M "),
+            'full_text': datetime.datetime.now().strftime(" %a %b %d %H:%M"),
+            'short_text': datetime.datetime.now().strftime(" %H:%M"),
             'icon': 'mmbar/icons/clock.xbm',
         }
